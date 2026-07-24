ec199a37992d8a41d206e3cbf48596646cca4e21
chintalerushikesh-ux <chintalerushikesh@gmail.com>
2026-07-24 10:57:31 +0530

    Add files via upload

---
+++ swap.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int a,b,c;
+cout<<"Enter the first number:"<<endl;
+cin>>a;
+cout<<"Enter the second number:"<<endl;
+cin>>b;
+c=a;
+a=b;
+b=c;
+cout<<"the value of a is "<<a <<"and the value of b is"<<b;
+return 0;
+}
+
+
